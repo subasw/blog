a2539cd9d327092476a5b3c5d1c1d2201487ccf0
subash <sadsubash123@gmail.com>
2019-08-06 18:13:31 +0530

    add detail view for blog post

--- bloge/urls.py
+++ bloge/urls.py
@@ -4,4 +4,6 @@ from .import views
 urlpatterns = [
     #http://127.0.0.1:8000/
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name="post_detail"),
+    
 ]
