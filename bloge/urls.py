52e6e73f87b84ea0fe0122ad5be9260bb19a4cc6
subash <sadsubash123@gmail.com>
2019-08-13 20:32:36 +0530

    add features to create new post from frontend

--- bloge/urls.py
+++ bloge/urls.py
@@ -4,6 +4,10 @@ from .import views
 urlpatterns = [
     #http://127.0.0.1:8000/
     path('', views.post_list, name='post_list'),
+
+    #http://127.0.0.1:8000/post/2
     path('post/<int:pk>/', views.post_detail, name="post_detail"),
-    
+
+    #http://127.0.0.1:8000/post/new
+    path('post/new/', views.post_new, name='post_new')
 ]
