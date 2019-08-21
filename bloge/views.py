50fd8bd3c51f20077ba4933e3e3f0c5c585d2d1e
subash <sadsubash123@gmail.com>
2019-08-21 12:14:08 +0530

    add login

--- bloge/views.py
+++ bloge/views.py
@@ -1,21 +1,37 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
+
 from django.utils import timezone
-
-from .forms import PostForm
 from .models import Post
-
+from .forms import PostForm
 # Create your views here.
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     stuff_for_frontend = {'posts': posts}
     return render(request, 'bloge/post_list.html', stuff_for_frontend)
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     stuff_for_frontend = {'post': post}
     return render(request, 'bloge/post_detail.html', stuff_for_frontend)
 
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+
+    else:
+        form = PostForm(instance=post)
+        stuff_for_frontend = {'form': form}
+    return render(request, 'bloge/post_edit.html', stuff_for_frontend)
 
 @login_required
 def post_new(request):
@@ -31,22 +47,6 @@ def post_new(request):
         stuff_for_frontend = {'form': form}
     return render(request, 'bloge/post_edit.html', stuff_for_frontend)
 
-
-@login_required
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == 'POST':
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-
-    else:
-        form = PostForm(instance=post)
-        stuff_for_frontend = {'form': form , 'post': post}
-    return render(request, 'bloge/post_edit.html', stuff_for_frontend)
 
 @login_required
 def post_draft(request):
