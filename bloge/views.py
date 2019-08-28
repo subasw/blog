fd58acf8149d4cd431c62bb7d2a21ad64fe63289
subash <sadsubash123@gmail.com>
2019-08-28 15:29:57 +0530

    add require to login to post

--- bloge/views.py
+++ bloge/views.py
@@ -61,7 +61,7 @@ def post_publish(request, pk):
     post.publish()
     return redirect('post_detail', pk=post.pk)
 
-
+@login_required
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
@@ -76,12 +76,14 @@ def add_comment_to_post(request, pk):
         form = CommentForm()
     return render(request, 'bloge/add_comment_to_post.html', {'form': form})
 
-
+@login_required
 def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
     return redirect('post_detail', pk=comment.post.pk)
 
+    
+@login_required
 def comment_approve(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
