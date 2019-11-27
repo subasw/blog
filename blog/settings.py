2acda315665417c0e2038ad2eb33ec18301fd591
subash <sadsubash123@gmail.com>
2019-11-27 20:05:01 +0530

    intial commit

--- blog/settings.py
+++ blog/settings.py
@@ -125,14 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
-STATICFILES_DIRS = [
-                os.path.join(BASE_DIR, "static"),
-            ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
