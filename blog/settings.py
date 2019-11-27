bb4fe8b24c63f166d3311c4f5333fa1f05ffcccc
subash <sadsubash123@gmail.com>
2019-11-27 20:28:49 +0530

    intial commit

--- blog/settings.py
+++ blog/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://cleverblog.herokuapp.com/']
 
 LOGIN_REDIRECT_URL = 'post_list'
 
@@ -125,6 +125,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
+STATICFILES_DIRS = [
+                os.path.join(BASE_DIR, "static"),
+            ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
